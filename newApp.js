437446da8aa6277605d854328e2b837caabf86f4
KARAKURT EMINE-RANA <eminerana.karakurt@etu.unistra.fr>
2022-04-14 15:19:58 +0200

    boutons de filtres + initialisation des filtres

--- newApp.js
+++ newApp.js
@@ -15,11 +15,14 @@ function afficheZoomImg(){
     image.src = imageSrc;
     document.body.appendChild(divImage);
     divImage.appendChild(image);
-
+    
+    console.log(image.style);
     /////////////////////////////////////
     //Fonctionnalité de zoom dans l'image
     ////////////////////////////////////
     var zoomScale = 1;
+    var brightness = 100;
+    var contrast = 100;
     let pos = { top: 0, left: 0, x: 0, y: 0 };
 
     const mouseDownHandler = function (e) {
@@ -51,23 +54,46 @@ function afficheZoomImg(){
         document.removeEventListener('mouseup', mouseUpHandler);
     };
     divImage.addEventListener('mousedown', mouseDownHandler);
-
     console.log('zoom actuel ', zoomScale);
+
     //Ecouteur d'évenements pour les boutons de zoom
     zoomBtn.addEventListener('click', function(){
         if(zoomScale < 15){
             zoomScale++;
             image.style.scale = zoomScale;
-            console.log(zoomScale);
         }
     });
     dezoomBtn.addEventListener('click', function(){
         if(zoomScale > 1){
             zoomScale--;
             image.style.scale = zoomScale;
-            console.log(zoomScale);
         }
     });
+
+    // Ecouteur d'évenements pour les boutons de luminosité
+    brightnessPlusBtn.addEventListener('click', function(){
+        brightness = brightness + 50;
+        image.style.filter = `brightness(${brightness}%)`;
+    });
+    brightnessMoinsBtn.addEventListener('click', function(){
+        brightness = brightness - 50;
+        image.style.filter = `brightness(${brightness}%)`;
+    });
+
+    //Ecouteur d'évenements pour les boutons de contraste
+    contrastPlusBtn.addEventListener('click', function(){
+        contrast = contrast + 50;
+        image.style.filter = `contrast(${contrast}%)`;
+    });
+    contrastMoinsBtn.addEventListener('click', function(){
+        contrast = contrast - 50;
+        image.style.filter = `contrast(${contrast}%)`;
+    });
+
+    //Tous les filtres sont enlevés de l'image
+    initializeBtn.addEventListener('click', function(){
+        image.style.removeProperty('filter');
+    })
 }
 
 //Variables utilisés dans la boucle
@@ -78,8 +104,11 @@ var parsed;
 var zoomBtn = document.querySelector("#zoom");
 var dezoomBtn = document.querySelector("#dezoom");
 var imageCarte = document.querySelector("#carte");
-
-
+var brightnessPlusBtn = document.querySelector("#brightnessPlus");
+var brightnessMoinsBtn = document.querySelector("#brightnessMoins");
+var contrastPlusBtn = document.querySelector("#contrastPlus");
+var contrastMoinsBtn = document.querySelector("#contrastMoins");
+var initializeBtn = document.querySelector("#initialize");
 //Requêtes Websocket 
 
 //Récupère le user id
@@ -121,11 +150,3 @@ ws.addEventListener('message',function(event){
             break;
     }
 });
-
-
-        
-
-        
-        
-
- 
